e3be4cb2063a71d609da08cc3eb6ceba7ef09f09
David Jiang <djiang86@binghamton.edu>
2018-05-17 18:46:03 +0000

    invoking subscribers once using once

---
+++ PDF/Backbonejs/js/Once-Event.js
@@ -0,0 +1,24 @@
+let ToDoCounter = {
+    counterA: 0,
+    counterB: 0
+};
+
+_.extend(ToDoCounter, Backbone.Event);
+
+let incrA = function () {
+    ToDoCounter.counterA++;
+
+    ToDoCounter.trigger('event');
+};
+
+let incrB = function () {
+    ToDoCounter.counterB++;
+};
+
+ToDoCounter.once('event', incrA);
+ToDoCounter.once('event', incrB);
+
+ToDoCounter.trigger('event');
+
+console.log(ToDoCounter.counterA === 1); // true
+console.log(ToDoCounter.counterB === 1); // true
