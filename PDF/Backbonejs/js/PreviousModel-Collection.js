0a195ee100bae7e89bf4a9214238e772b8767e9f
David Jiang <djiang86@binghamton.edu>
2018-05-17 19:32:23 +0000

    retrieving previous date after resetting

---
+++ PDF/Backbonejs/js/PreviousModel-Collection.js
@@ -0,0 +1,13 @@
+// Also note that listening to a reset event, the list of previous models is available
+// in options.previousModels
+
+let todo = new Backbone.Model();
+
+let todos = new Backbone.Collection([todo])
+                .on('reset', function (todos, options) {
+                    console.log(options.previousModels);
+                    console.log([todo]);
+                    console.log(options.previousModels[0] === todo); // true
+                });
+
+todos.reset();
--- PDF/Backbonejs/js/Resetting-Collection.js
+++ PDF/Backbonejs/js/Resetting-Collection.js
@@ -18,3 +18,8 @@ TodosCollection.reset([
 
 // Above logs 'Collection reset.'
 console.log('Collection size: ' + TodosCollection.length); // Collection size: 1
+
+
+// Another useful tip is to use reset with no arguments to clear out a collection
+// completely. This is handy when dynamically loading a new page of results where
+// you want to blank out the current page of results.
