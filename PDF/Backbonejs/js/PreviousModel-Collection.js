d8af1e14f904b01cf2bf67251f315d2c5d4fb8fd
David Jiang <djiang86@binghamton.edu>
2018-05-17 19:40:33 +0000

    smart set feature for collection

--- PDF/Backbonejs/js/PreviousModel-Collection.js
+++ PDF/Backbonejs/js/PreviousModel-Collection.js
@@ -1,7 +1,12 @@
 // Also note that listening to a reset event, the list of previous models is available
 // in options.previousModels
 
-let todo = new Backbone.Model();
+let todo = new Backbone.Model({
+    defaults: {
+        title: 'example',
+        id: 2
+    }
+});
 
 let todos = new Backbone.Collection([todo])
                 .on('reset', function (todos, options) {
