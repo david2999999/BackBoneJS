71947782f5e9a5ded2952d2f8a695d93d4720ae5
David Jiang <djiang86@binghamton.edu>
2018-05-18 15:13:19 +0000

    removing models from collection and server

---
+++ PDF/Backbonejs/js/Restful-Persistence/Destroy.js
@@ -0,0 +1,21 @@
+// A model can be removed from the containing collection and the server by calling
+// its destroy() method. Unlike Collection.remove() which only removes a
+// model from a collection, Model.destroy() will also send an HTTP DELETE
+// to the collection’s URL.
+
+let Todo = Backbone.Model.extend({
+    defaults: {
+        title: '',
+        completed: false
+    }
+});
+
+let TodosCollection = Backbone.Collection.extend({
+    model: Todo,
+    url: '/todos'
+});
+let todos = new TodosCollection();
+todos.fetch();
+
+let todo2 = todos.get(2);
+todo2.destroy(); // sends HTTP DELETE to /todos/2 and removes from collection
