fedb7258736af5fb7bf8f450d07b0dad4e7f750e
David Jiang <djiang86@binghamton.edu>
2018-05-21 13:39:17 +0000

    creating the collection and model view

---
+++ PDF/Book-Library-App/site/js/views/book.js
@@ -0,0 +1,13 @@
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'bookContainer',
+    template: _.template($('#bookTemplate').html()),
+
+    render: function () {
+        this.$el.html(this.template(this.model.attributes));
+        
+        return this;
+    }
+});
