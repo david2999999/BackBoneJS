f2f05912646eebd8fb41e3c4dbd6ef43e9f54a2b
David Jiang <djiang86@binghamton.edu>
2018-05-21 14:19:58 +0000

    added functionality of adding a book

--- PDF/Book-Library-App/site/js/views/library.js
+++ PDF/Book-Library-App/site/js/views/library.js
@@ -9,6 +9,7 @@ app.LibraryView = Backbone.View.extend({
     initialize: function (initialBooks) {
         this.collection = new app.Library(initialBooks);
         this.render();
+        this.listenTo( this.collection, 'add', this.renderBook );
     },
 
     render: function() {
@@ -23,5 +24,55 @@ app.LibraryView = Backbone.View.extend({
         });
 
         this.$el.append(bookView.render().el);
+    },
+
+    events: {
+        'click #add' : 'addBook'
+    },
+
+    addBook: function ( e ) {
+        /*Backbone passes an event object as a parameter to the event-handling function.
+        This is useful for us in this case since we don’t want the form to actually submit
+        and reload the page. Adding a call to preventDefault on the event in the
+        addBook function takes care of this for us.*/
+        e.preventDefault();
+
+        var formData = {};
+
+        /*We select all the input elements of the form that have a value and iterate over
+        them using jQuery’s each. Since we used the same names for ids in our form as
+        the keys on our Book model, we can simply store them directly in the formData
+        object. We then create a new Book from the data and add it to the collection.
+        We skip fields without a value so that the defaults will be applied.*/
+        $( '#addBook div' ).children('input').each(function (i , el) {
+            if($(el).val() != ''){
+                formData[el.id] = $(el).val();
+            }
+        });
+
+        this.collection.add(new app.Book(formData));
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
