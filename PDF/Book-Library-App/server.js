5ce2075f496f535e2a60029756cc21d23c95b219
David Jiang <djiang86@binghamton.edu>
2018-05-21 17:26:00 +0000

    created package json file to connect to express and mongodb

---
+++ PDF/Book-Library-App/server.js
@@ -0,0 +1,56 @@
+// Module dependencies.
+var application_root = __dirname,
+    express = require( 'express' ), //Web framework
+    bodyParser = require('body-parser'), //Parser for reading request body
+    path = require( 'path' ), //Utilities for dealing with file paths
+    mongoose = require( 'mongoose' ); //MongoDB integration
+
+//Create server
+var app = express();
+
+//Where to serve static content
+app.use( express.static( path.join( application_root, 'site') ) );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//Start server
+var port = 4711;
+
+app.listen( port, function() {
+    console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
+});
+
+//Connect to database
+mongoose.connect( 'mongodb://localhost/library_database' );
+
+//Schemas
+var Book = new mongoose.Schema({
+    title: String,
+    author: String,
+    releaseDate: Date
+});
+
+//Models
+var BookModel = mongoose.model( 'Book', Book );
+
+// Configure server
+app.configure( function() {
+    //parses request body and populates request.body
+    app.use( express.bodyParser() );
+
+    //checks request.body for HTTP method overrides
+    app.use( express.methodOverride() );
+
+    //perform route lookup based on url and HTTP method
+    app.use( app.router );
+
+    //Where to serve static content
+    app.use( express.static( path.join( application_root, 'site') ) );
+
+    //Show all errors in development
+    app.use( express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.get( '/api', function( request, response ) {
+    response.send( 'Library API is running' );
+});
