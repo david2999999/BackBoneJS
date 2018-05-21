180b1198f756a51c8add04fa388ba7b08912aa86
David Jiang <djiang86@binghamton.edu>
2018-05-21 19:19:34 +0000

    delete functionality and also added keywords to models

--- PDF/Book-Library-App/server.js
+++ PDF/Book-Library-App/server.js
@@ -26,10 +26,16 @@ app.listen( port, function() {
 mongoose.connect( 'mongodb://localhost/library_database' );
 
 //Schemas
+var Keywords = new mongoose.Schema({
+    keyword: String
+});
+
+//Schemas
 var Book = new mongoose.Schema({
     title: String,
     author: String,
-    releaseDate: Date
+    releaseDate: Date,
+    keywords: [ Keywords ]
 });
 
 //Models
@@ -78,7 +84,8 @@ app.post( '/api/books', function( request, response ) {
     var book = new BookModel({
         title: request.body.title,
         author: request.body.author,
-        releaseDate: request.body.releaseDate
+        releaseDate: request.body.releaseDate,
+        keywords: request.body.keywords
     });
 
     return book.save( function( err ) {
@@ -116,10 +123,28 @@ app.put( '/api/books/:id', function( request, response ) {
         book.title = request.body.title;
         book.author = request.body.author;
         book.releaseDate = request.body.releaseDate;
+        book.keywords = request.body.keywords;
+
         return book.save( function( err ) {
             if( !err ) {
                 console.log( 'book updated' );
                 return response.send( book );
+            } else {
+                console.log( err );
+            }
+        });
+    });
+});
+
+
+//Delete a book
+app.delete( '/api/books/:id', function( request, response ) {
+    console.log( 'Deleting book with id: ' + request.params.id );
+    return BookModel.findById( request.params.id, function( err, book ) {
+        return book.remove( function( err ) {
+            if( !err ) {
+                console.log( 'Book removed' );
+                return response.send( '' );
             } else {
                 console.log( err );
             }
